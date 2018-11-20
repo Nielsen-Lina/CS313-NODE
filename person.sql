0d9b90d8c68ce38098e83918b395d5af4c31c5be
Lina Nielsen <nie15031@byui.edu>
2018-11-20 14:31:40 -0500

    Modified person.ejs for AJAX

--- person.sql
+++ person.sql
@@ -13,10 +13,11 @@ CREATE TABLE relationship(
 	,primary key(parent, child)
 );
 
-INSERT INTO person(first_name, last_name, dob) VALUES ('Child 1', 'Nielson', '2009-01-01'), ('Child 2', 'Nielson', '2010-05-01'), ('Parent 1', 'Nielson', '1980-01-01')
+INSERT INTO person(first_name, last_name, dob) VALUES ('Child 1', 'Nielson', '2009-01-01'), ('Child 2', 'Nielson', '2010-05-01'), ('Child 3', 'Nielson', '2010-06-01'), ('Parent 1', 'Nielson', '1980-01-01')
 , ('Parent 2', 'Nielson', '1975-01-01');
 INSERT INTO relationship(child, parent) VALUES ((select id FROM person WHERE first_name = 'Child 1'), (select id FROM person WHERE first_name = 'Parent 1'));
 INSERT INTO relationship(child, parent) VALUES ((select id FROM person WHERE first_name = 'Child 2'), (select id FROM person WHERE first_name = 'Parent 2'));
+INSERT INTO relationship(child, parent) VALUES ((select id FROM person WHERE first_name = 'Child 3'), (select id FROM person WHERE first_name = 'Parent 2'));
 
 SELECT * FROM person;
 SELECT * FROM relationship;
